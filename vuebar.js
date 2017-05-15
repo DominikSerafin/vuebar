1356e958e781cf0bd9cf28d86febdf04a41d9236
Dominik Serafin <dominikdsgnr@gmail.com>
2017-05-15 14:56:24 +0200

    compatibility improvements and allow only left mouse button to drag dragger fix

--- vuebar.js
+++ vuebar.js
@@ -1,30 +1,8 @@
 /*----------------------------------------*\
-
     Vuebar
-
-    TODO:
-
-    * Dragger drag only with left click
-
-    * Fix refresh sometimes not firing on child components update
-    * Fix position of el2 on mobile browser where there aren't any scrollbars (overflow-y: scroll thing)
-
-    * Option to dynamically enable/disable scrollbar
-
-    * Support for MutationObserver refresh
-    * Support for touch dragging dragger
-    * Support for touch preventing parent scroll
-    * Support for horizontal scrolling
-    * Support for refresh on dynamic content (like img) load
-    * Support for refresh on page orientationchange
-    * Support for lass compatibility with component scoped stylesheets
-
 \*----------------------------------------*/
 ;(function(){
     'use strict'
-
-
-
 
 
 
@@ -34,7 +12,6 @@
     \*------------------------------------*/
     var VueBar = {};
     VueBar.install = function(Vue, options){
-
 
 
 
@@ -105,18 +82,14 @@
         };
 
 
+
+
         /*------------------------------------*\
             Get State
         \*------------------------------------*/
         function getState(el){
             return el._vuebarState;
         };
-
-
-
-
-
-
 
 
 
@@ -139,14 +112,9 @@
 
 
 
-
-
-
-
         /*------------------------------------*\
             Computing Properties
         \*------------------------------------*/
-
         function computeVisibleArea(el){
             var state = getState(el);
             state.visibleArea = (state.el2.clientHeight / state.el2.scrollHeight);
@@ -195,13 +163,6 @@
 
 
 
-
-
-
-
-
-
-
         /*------------------------------------*\
             Styles & DOM
         \*------------------------------------*/
@@ -224,18 +185,15 @@
         };
 
 
-
         function updateDragger(el, options){
             var options = options ? options : {};
             var state = getState(el);
-
 
             // setting dragger styles
             state.dragger.style.height = parseInt( Math.round( state.barHeight)  ) + 'px';
             state.dragger.style.top = parseInt( Math.round( state.barTop ) ) + 'px';
             //state.dragger.style.height = Math.ceil( state.barHeight ) + 'px';
             //state.dragger.style.top = Math.ceil( state.barTop ) + 'px';
-
 
             // scrollbar visible / invisible classes
             if (state.visibleArea<1) {
@@ -311,17 +269,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
         /*------------------------------------*\
             Refresh
         \*------------------------------------*/
@@ -343,18 +290,6 @@
                 updateDragger(el);
             }.bind(this));
         };
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -382,7 +317,6 @@
                 preventParentScroll(el, event);
             }.bind(this);
         };
-
 
 
         function documentMousemove(el){
@@ -423,10 +357,12 @@
         };
 
 
-
         function barMousedown(el){
             var state = getState(el);
             return function(event){
+
+                // don't do nothing if it's not left mouse button
+                if (event.which!==1){return false};
 
                 state.barDragging = true;
                 state.mouseBarOffsetY = event.layerY;
@@ -450,28 +386,12 @@
         };
 
 
-
         function windowResize(el){
             var state = getState(el);
             return debounce(function(event){
                 refreshScrollbar(el, {resize: true});
             }.bind(this), state.config.resizeDebounce)
         };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -515,12 +435,12 @@
             state.el1.style.overflow = 'hidden';
 
             // el2 styles
-            var edge = window.navigator.userAgent.indexOf('Edge') > -1;
             state.el2.style.overflowX = 'hidden';
             state.el2.style.overflowY = 'scroll';
             state.el2.style.msOverflowStyle = 'scrollbar';
             state.el2.style.height = '100%';
-            state.el2.style.width = 'calc(100% + '+ (edge ? 12 : 17) +'px)';
+            var widthPixels = uaBrowser().mobile ? 0 : ( uaBrowser().edge ? 12 : 17 );
+            state.el2.style.width = 'calc(100% + ' + widthPixels + 'px)';
 
             // add events
             // - wheel event is only needed when preventParentScroll option is enabled
@@ -534,14 +454,6 @@
             refreshScrollbar(el, {init: true});
 
         };
-
-
-
-
-
-
-
-
 
 
 
@@ -573,14 +485,6 @@
 
 
 
-
-
-
-
-
-
-
-
         /*------------------------------------*\
             Debounce Helper
             https://remysharp.com/2010/07/21/throttling-function-calls
@@ -595,7 +499,6 @@
                 }, delay);
             };
         };
-
 
 
 
@@ -630,7 +533,6 @@
 
 
 
-
         /*------------------------------------*\
             Class Manipulation Helper
             https://plainjs.com/javascript/attributes/adding-removing-and-testing-for-classes-9/
@@ -652,9 +554,8 @@
 
 
 
-
         /*------------------------------------*\
-            Style Vendor Prefixes
+            Style Vendor Prefixes Helper
         \*------------------------------------*/
         function compatStyle(element, property, value) {
             element.style["webkit" + property] = value;
@@ -665,6 +566,25 @@
         }
 
 
+
+
+        /*------------------------------------*\
+            Browser Detection Helper
+        \*------------------------------------*/
+        function uaBrowser(){
+
+            var useragent = navigator.userAgent || navigator.vendor || window.opera;
+            var edge = window.navigator.userAgent.indexOf('Edge') > -1;
+
+            // regex below thanks to http://detectmobilebrowsers.com/
+            var mobile = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(useragent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(useragent.substr(0,4)));
+
+            return {
+                edge: edge,
+                mobile: mobile,
+            }
+
+        }
 
 
 
@@ -702,7 +622,6 @@
 
 
 
-
     /*------------------------------------*\
         Autoinstall
     \*------------------------------------*/
@@ -717,7 +636,6 @@
     if (typeof Vue !== 'undefined') {
         Vue.use(VueBar)
     }
-
 
 
 
